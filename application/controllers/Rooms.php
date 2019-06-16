436484d0d7dcc8f18e01164116fb1e83748c5d4f
sonalibhagwat <sonali.m.bhagwat@gmail.com>
2019-06-16 18:01:43 +0530

    roomtypes add form submit

--- application/controllers/Rooms.php
+++ application/controllers/Rooms.php
@@ -6,12 +6,40 @@ Class Rooms extends CI_Controller{
     public function index()
 	{
 		// $this->load->view('dashboard');
-
+        $data['roomtypes'] = $this->Rooms_model->get_roomtypes();
 		// $data['title'] = $data['post']['title'];
         $this->load->view('templates/header');
-        $this->load->view('pages/roomtypes');
+        $this->load->view('pages/roomtypes',$data);
         $this->load->view('templates/footer');
-	}
+    }
+    
+   
+		public function create(){
+            // echo "in create" ;
+			// Check login
+			// if(!$this->session->userdata('logged_in')){
+			// 	redirect('users/login');
+			// }
+
+			// $data['title'] = 'Create Category';
+
+			$this->form_validation->set_rules('roomtypeName', 'Room Type', 'required');
+
+			if($this->form_validation->run() === FALSE){
+            $data['roomtypes'] = $this->Rooms_model->get_roomtypes();
+                
+                $this->load->view('templates/header');
+				$this->load->view('pages/roomtypes',$data);
+				$this->load->view('templates/footer');
+			} else {
+				$this->Rooms_model->create_roomtype();
+
+			// 	// Set message
+			// 	// $this->session->set_flashdata('category_created', 'Your category has been created');
+
+				redirect('Rooms');
+			}
+		}
 }
 
 
