4b71b15e2d3fc9b26c43951c7079e52d4b481a97
sonalibhagwat <sonali.m.bhagwat@gmail.com>
2019-06-22 18:32:09 +0530

    document type add ,edit,delete function updated

--- application/controllers/Documents.php
+++ application/controllers/Documents.php
@@ -5,8 +5,7 @@ Class Documents extends CI_Controller{
 
     public function index()
 	{
-		// $this->load->view('dashboard');
-		// $data['title'] = $data['post']['title'];
+		
         $data['DocumentType'] = $this->Documents_model->get_documenttypes();
         $this->load->view('templates/header');
         $this->load->view('pages/documentstypes',$data);
@@ -22,71 +21,49 @@ Class Documents extends CI_Controller{
 				return false;
 			}
 		}
-    public function create(){
-        	// Check login
-			// if(!$this->session->userdata('logged_in')){
-			// 	redirect('users/login');
-			// }
 
-			// $data['title'] = 'Create Category';
 
-			$this->form_validation->set_rules('documenttypeName', 'Document Type', 'required|callback_check_type_exists');
-
-			if($this-Payment_model>form_validation->run() === FALSE){
-            $data['DocumentType'] = $this->Documents_model->get_documenttypes();
-                
-                $this->load->view('templates/header');
-				$this->load->view('pages/documentstypes',$data);
-				$this->load->view('templates/footer');
-			} else {
-				$this->Documents_model->create_documentstypes();
-
-			     // 	// Set message
-			     // 	// $this->session->set_flashdata('category_created', 'Your category has been created');
-
-				redirect('Documents');
-			}
-
-	}
+		public function getTypes(){
+			$data = $this->Documents_model->get_documenttypes();
+			echo json_encode($data);
+		}
 	
 
-	public function update(){
-		// if(!$this->session->userdata('logged_in')){
-		//   redirect('users/login');
-		// }
-		// echo "Updated";
+	
 
-
-		$this->form_validation->set_rules('documenttypeName1', 'Document Type', 'required|callback_check_type_exists');
-
-		if($this->form_validation->run() === FALSE){
-		$data['DocumentType'] = $this->Documents_model->get_documenttypes();
-			
-			$this->load->view('templates/header');
-			$this->load->view('pages/documentstypes',$data);
-			$this->load->view('templates/footer');
+	public function create(){
+		$type = $this->input->post('typename');
+		$ret = $this->check_type_exists($type);
+			if($ret === false){
+			$response['msg'] = false; 
+			echo json_encode($response);
 		} else {
-			$this->Documents_model->update_type();//call to model function update_post
-	   // $this->session->set_flashdata('post_updated', 'Your Post has been updated');
-  
-			  redirect('Documents');
-		}
-   
-		
-	  }
+			$this->Documents_model->create_documentstypes();
+			$response['msg'] = true; 
+			echo json_encode($response);
+			 // 	// Set message
+			 // 	// $this->session->set_flashdata('category_created', 'Your category has been created');
 
-	  public function delete($id){
-		// if(!$this->session->userdata('logged_in')){
-		//   redirect('users/login');
-		// }
-  
-		$this->Documents_model->delete_type($id);
-		// $this->session->set_flashdata('post_deleted', 'Your Post has been Deleted');
-  
+		}
+
+	}
+
+
+ 	
+	  public function update(){
+		$this->Documents_model->update_type();//call to model function update_post
+		$response['msg'] = true; 
+		echo json_encode($response);
+	}
+
+
+	  public function delete(){
+		$typeid = $this->input->post('typeid');
+		$this->Documents_model->delete_type($typeid);
 		redirect('Documents');
-	  }
+	}
 
 }
 
-Payment_model
+
 ?>
--- application/views/pages/roomtypes.php
+++ application/views/pages/roomtypes.php
@@ -54,87 +54,12 @@
 
                         </form>
                         
-                        <!-- <form class="form-horizontal" action="<?php echo site_url();?>/rooms/create" class="form-horizontal" method="post">
- 
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <h3 class="panel-title"><strong>Room Types</strong> Add New</h3>
-                                <ul class="panel-controls">
-                                    <li><a href="#" class="panel-remove"><span class="fa fa-times"></span></a></li>
-                                </ul>
-                            </div>
-                            <div class="panel-body">
-                                <p></p>
-                            </div>
-                            <div class="panel-body">                                                                        
-                                
-                                <div class="form-group">
-                                    <label class="col-md-3 col-xs-12 control-label">Room Type</label>
-                                    
-                                    <div class="col-md-6 col-xs-12">                                            
-                                        <div class="input-group">
-                                            <span class="input-group-addon"><span class="fa fa-pencil"></span></span>
-                                            <input type="text" class="form-control" name="roomtypeName"/>
-                                        </div>        
-
-                                        <span class="help-block">Add Room Type as Single,Double etc.</span>&nbsp;<?php echo validation_errors(); ?>
-                                    </div>
-                                </div>
-                       
-                            </div>
-                            <div class="panel-footer">
-                                <button class="btn btn-default" type="reset">Clear Form</button>                                    
-                                <button type="submit" class="btn btn-primary pull-right">Submit</button>
-                            </div>
-                        </div>
-
-                        </form> -->
+                      
                         
                     </div>
 
 
-                    <!-- <div class="col-md-12"  id="div_editFrm" style="display:none">
-                        
-                        <form class="form-horizontal" action="<?php echo site_url();?>/rooms/update" class="form-horizontal" method="post">
- 
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <h3 class="panel-title"><strong>Room Types</strong> Add New</h3>
-                                <ul class="panel-controls">
-                                    <li><a href="#" class="panel-remove"><span class="fa fa-times"></span></a></li>
-                                </ul>
-                            </div>
-                            <div class="panel-body">
-                                <p></p>
-                            </div>
-
-                            <input type="hidden" id="id" name="id"/>
-
-                            <div class="panel-body">                                                                        
-                                
-                                <div class="form-group">
-                                    <label class="col-md-3 col-xs-12 control-label">Room Type</label>
-                                    
-                                    <div class="col-md-6 col-xs-12">                                            
-                                        <div class="input-group">
-                                            <span class="input-group-addon"><span class="fa fa-pencil"></span></span>
-                                            <input type="text" class="form-control" id="roomtypeName1" name="roomtypeName1"/>
-                                        </div>        
-
-                                        <span class="help-block">Add Room Type as Single,Double etc.</span>&nbsp;<?php echo validation_errors(); ?>
-                                    </div>
-                                </div>
-                       
-                            </div>
-                            <div class="panel-footer">
-                                <button class="btn btn-default" type="reset">Clear Form</button>                                    
-                                <button type="submit" class="btn btn-primary pull-right">Save</button>
-                            </div>
-                        </div>
-
-                        </form>
-                        
-                    </div> -->
+                 
 
                 </div>      
                 
@@ -156,21 +81,7 @@
                                         </thead>
                                         <tbody id="Tbl_roomtypes_body">
                                         
-                                            <!-- <?php foreach($roomtypes as $rt):?>
-                                            <tr>
-                                                <td><?php echo ucfirst($rt['roomType']);?></td>
-                                                
-                                                <td><?php echo $rt['created_at'];?></td>
-                                                <td><div class="btn-group btn-group-sm"> 
-                                                <button class="btn btn-default btn-rounded btn-sm" onclick='edit_row("<?php echo $rt['roomId']; ?>" , "<?php echo $rt['roomType'];?>");' ><span class="fa fa-pencil"></span></button>
-                                                <?php echo form_open('/Rooms/delete/'.$rt['roomId']); ?>
-                                                
-                                                <button class="btn btn-danger btn-rounded btn-sm" ><span class="fa fa-times"></span></button>
-                                                </form>
-                                                
-                                                </div></td>
-                                            </tr>
-                                    <?php endforeach;?> -->
+                                           
                                         </tbody>
 </table> 
 </div>
