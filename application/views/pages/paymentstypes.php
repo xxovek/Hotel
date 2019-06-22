37147806312935892a10add7bdd3a99d8d5936f9
sonalibhagwat <sonali.m.bhagwat@gmail.com>
2019-06-22 16:35:54 +0530

    update datatable format pushed

--- application/views/pages/paymentstypes.php
+++ application/views/pages/paymentstypes.php
@@ -60,8 +60,38 @@
 
                 </div>      
                 
+
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">DataTable Export</h3>
+                        <!-- <div class="btn-group pull-right">
+                            <button class="btn btn-success" onclick="window.location='Customer/add_customer'"><i class="fa fa-bars"></i>New Customer</button>
+                        </div> -->
+                        <div class="btn-group pull-right">
+                            <button class="btn btn-danger dropdown-toggle" data-toggle="dropdown"><i class="fa fa-bars"></i> Export Data</button>
+                            <ul class="dropdown-menu">
+                                <li><a href="#" onClick="$('#Tbl_paymenttypes').tableExport({type:'json',escape:'false'});"><img src='<?php echo base_url(); ?>img/icons/json.png' width="24" /> JSON</a></li>
+                                <li><a href="#" onClick="$('#Tbl_paymenttypes').tableExport({type:'json',escape:'false',ignoreColumn:'[2,3]'});"><img src='<?php echo base_url(); ?>img/icons/json.png' width="24" /> JSON (ignoreColumn)</a></li>
+                                <li><a href="#" onClick="$('#Tbl_paymenttypes').tableExport({type:'json',escape:'true'});"><img src='<?php echo base_url(); ?>img/icons/json.png' width="24" /> JSON (with Escape)</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#" onClick="$('#Tbl_paymenttypes').tableExport({type:'xml',escape:'false'});"><img src='<?php echo base_url(); ?>img/icons/xml.png' width="24" /> XML</a></li>
+                                <li><a href="#" onClick="$('#Tbl_paymenttypes').tableExport({type:'sql'});"><img src='<?php echo base_url(); ?>img/icons/sql.png' width="24" /> SQL</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#" onClick="$('#Tbl_paymenttypes').tableExport({type:'csv',escape:'false'});"><img src='<?php echo base_url(); ?>img/icons/csv.png' width="24" /> CSV</a></li>
+                                <li><a href="#" onClick="$('#Tbl_paymenttypes').tableExport({type:'txt',escape:'false'});"><img src='<?php echo base_url(); ?>img/icons/txt.png' width="24" /> TXT</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#" onClick="$('#Tbl_paymenttypes').tableExport({type:'excel',escape:'false'});"><img src='<?php echo base_url(); ?>img/icons/xls.png' width="24" /> XLS</a></li>
+                                <li><a href="#" onClick="$('#Tbl_paymenttypes').tableExport({type:'doc',escape:'false'});"><img src='<?php echo base_url(); ?>img/icons/word.png' width="24" /> Word</a></li>
+                                <li><a href="#" onClick="$('#Tbl_paymenttypes').tableExport({type:'powerpoint',escape:'false'});"><img src='<?php echo base_url(); ?>img/icons/ppt.png' width="24" /> PowerPoint</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#" onClick="$('#Tbl_paymenttypes').tableExport({type:'png',escape:'false'});"><img src='<?php echo base_url(); ?>img/icons/png.png' width="24" /> PNG</a></li>
+                                <li><a href="#" onClick="$('#Tbl_paymenttypes').tableExport({type:'pdf',escape:'false'});"><img src='<?php echo base_url(); ?>img/icons/pdf.png' width="24" /> PDF</a></li>
+                            </ul>
+                        </div>
+
+                    </div>
                 <div class="panel-body">
-                                    <table id="Tbl_paymenttypes" class="table datatable">
+                                    <table id="Tbl_paymenttypes" class="table">
                                         <thead>
                                             <tr>
                                                 <!-- <th>#</th> -->
@@ -75,8 +105,11 @@
                                         <tbody id="Tbl_paymenttypes_body">
                                           
                                          </tbody>
-</table> 
-</div>
+                                    </table> 
+                              </div>
+
+                </div>
+
 <!-- onclick="edit_row('<?php echo $pt['paymentTypeId'];?>'',''<?php echo $pt['paymentType'];?>');" -->
             </div>
             <!-- END PAGE CONTENT WRAPPER -->       
