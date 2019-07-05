cb271211d57df008de3f843b834b9347bb5c1f6e
vbpawar <vikaspawar3110@gmail.com>
2019-07-05 16:10:28 +0530

    make on changes

--- application/controllers/Booking.php
+++ application/controllers/Booking.php
@@ -5,10 +5,6 @@ Class Booking extends CI_Controller{
 
     public function index()
 	{
-		// $this->load->view('dashboard');
-
-		// $data['title'] = $data['post']['title'];
-
         $this->load->view('templates/header');
         $this->load->view('pages/booking');
         $this->load->view('templates/footer');
